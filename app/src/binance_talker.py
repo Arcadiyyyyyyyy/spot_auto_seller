889724214f12c6224df5b948d3052e1fe2111180
Anton <109826223+Arcadiyyyyyyyy@users.noreply.github.com>
2023-09-20 09:47:04 +0100

    Squashed commit of the following:
    commit 18fd348b44fece05f920041be96c9db67daaf020
    Author: Slava Steshchenko <136891597+vsteschenko@users.noreply.github.com>
    Date:   Wed Sep 20 10:42:39 2023 +0200
    
        refactor: [#7] improved list comrehantion
    
    commit 5834e9fbf8cc120e90856b858e8311fb478b8571
    Author: Anton <109826223+Arcadiyyyyyyyy@users.noreply.github.com>
    Date:   Tue Sep 19 23:08:40 2023 +0100
    
        fix: [#7] improve handling data from the binance

--- app/src/binance_talker.py
+++ app/src/binance_talker.py
@@ -12,6 +12,7 @@ class BinanceConnector:
 
     def get_account_data(self):
         tickers_for_search = self._get_spot_balance()
+        tickers_for_search = self._clean_tickers_list(tickers_for_search)
         return self._get_tickers_price(tickers_for_search)
 
     @staticmethod
@@ -20,7 +21,7 @@ class BinanceConnector:
         api_secret = environ.get("BINANCE_API_SECRET")
         return Client(api_public, api_secret)
 
-    def _get_spot_balance(self) -> list:
+    def _get_spot_balance(self) -> list[dict]:
         assets_that_cost_more_than_x = []
 
         for asset in self.c.get_user_asset():
@@ -33,11 +34,16 @@ class BinanceConnector:
 
         return assets_that_cost_more_than_x
 
-    def _get_tickers_price(self, tickers_to_search: list[str]):
-        pairs = []
+    def _clean_tickers_list(self, tickers_list: list[dict]):
+    	spot_pairs = [x.get("symbol") for x in self.c.get_exchange_info().get("symbols")]
+    	return [x for x in tickers_list if f"{x.get('symbol', '')}USDT" in spot_pairs]
 
-        for symbol in tickers_to_search:
+    def _get_tickers_price(self, tickers_to_search: list[dict]):
+        result_pairs = []
+        pairs_to_search = [f"{x.get('asset')}USDT" for x in tickers_to_search]
+
+        for symbol, deposit_info in zip(pairs_to_search, tickers_to_search):
             ticker = self.c.get_ticker(symbol=symbol)
-            pairs.append(ticker)
+            result_pairs.append({**ticker, **deposit_info})
 
-        return pairs
+        return result_pairs
